8e0d75680a1eea433988e40127167db7c010277a
RenanCostaa <renancosta@ucl.br>
2020-08-22 01:38:03 +0000

    Inicio do exercicio

--- main.cs
+++ main.cs
@@ -1,7 +1,9 @@
 using System;
-
+//4. Faça um Programa que peça as 4 notas bimestrais e mostre a média
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+
+  public static void Main () {
+
   }
 }
