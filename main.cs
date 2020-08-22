de8a2b7a45448f81bb80989fd81c17f8bc8d4abf
RenanCostaa <renancosta@ucl.br>
2020-08-22 01:44:05 +0000

    estrutura montada; ainda sem funcao

--- main.cs
+++ main.cs
@@ -1,9 +1,25 @@
 using System;
 //4. Faça um Programa que peça as 4 notas bimestrais e mostre a média
 class MainClass {
+  public static void media(float a,float b,float c,float d){
 
+  }
 
   public static void Main () {
+    float uno, dos, tres, cuatro;
+
+    Console.Write("Insira a primeira nota bim.: ");
+    uno = float.Parse(Console.ReadLine());
+
+    Console.Write("Insira a segunda nota bim.: ");
+    dos = float.Parse(Console.ReadLine());
+
+    Console.Write("Insira a terceira nota bim.: ");
+    tres = float.Parse(Console.ReadLine());
+
+    Console.Write("Insira a quarta nota bim.: ");
+    cuatro = float.Parse(Console.ReadLine());
+
 
   }
 }
